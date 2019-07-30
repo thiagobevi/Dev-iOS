4fdb6daa633713805fca4e501d410b5df87ffc42
Thiago Bevilacqua <thiagobevi@hotmail.com>
2019-07-30 10:20:02 -0300

    Enviar dados como objeto pra proxima tela

---
+++ Filmes/Filmes/DetalhesFilmeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetalhesFilmeViewController.swift
+//  Filmes
+//
+//  Created by Thiago Bevilacqua on 7/29/19.
+//  Copyright © 2019 Thiago Bevilacqua. All rights reserved.
+//
+
+import Foundation
